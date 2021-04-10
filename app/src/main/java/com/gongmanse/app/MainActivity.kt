38f24fab825b32dac670885536639a5d7fad13b7
startwo09 <startwo09@gmail.com>
2021-04-10 23:48:10 +0900

    Commit and Push test of devStartwo09

--- app/src/main/java/com/gongmanse/app/MainActivity.kt
+++ app/src/main/java/com/gongmanse/app/MainActivity.kt
@@ -13,6 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate, branch name is devStartwo09 ")
+        Log.d(TAG, "onCreate, branch => devStartwo09 ")
     }
 }
